b960de69626bd2f7018ff52bb255f2a7acc5c309
romeritomorais.sisx <TR034803@comgas.com.br>
2022-04-21 01:32:12 -0300

    add notebook

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 sudo apt install tree
 
-if [ -d /opt/spark-3.1.2 ];
+if [ -d /opt/apache-spark-3.2.0 ];
 then
   echo "spark-3.1.2 já Instalado ... "
 else
-   rm -rf /usr/local/spark-3.1.2
-   rm -rf /opt/apache-spark-3.1.2
+   rm -rf /usr/local/spark-3.2.0 
+   rm -rf /opt/apache-spark-3.2.0 
    cd 30-day-challenges-with-spark && cd src/install
    chmod +x install-spark-3-2-0-with-pandas.sh
    ./install-spark-3-2-0-with-pandas.sh
