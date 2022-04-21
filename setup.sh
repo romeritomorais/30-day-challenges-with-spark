2bfd1d45f73e055140d62fb2c6421fe85f6990b9
romeritomorais.sisx <TR034803@comgas.com.br>
2022-04-21 00:51:54 -0300

    add notebook

---
+++ setup.sh
@@ -0,0 +1,5 @@
+rm -rf /usr/local/spark-3.1.2
+rm -rf /opt/apache-spark-3.1.2
+cd 30-day-challenges-with-spark && cd src/install
+chmod +x install-spark-3-2-0-with-pandas.sh
+./install-spark-3-2-0-with-pandas.sh
